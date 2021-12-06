f4c67da581f8b1317b42f447381ec424a50a5983
FrancescoGalanti <galanti_francesco>
2021-12-06 21:27:58 +0100

    firstcommit

---
+++ Description.md
@@ -0,0 +1,10 @@
+This is an amazing Calculator
+
+
+We really need it
+
+How to use it...
+
+""""
+td_sum(4,6)
+""""
---
+++ calculator.py
@@ -0,0 +1,15 @@
+def td_sum(x, y):
+    return x + y
+
+
+def td_divide(x, y):
+    return x / y
+
+
+def td_multiply(x, y):
+    return x * y
+
+
+def td_sub(x, y):
+    return x - y
+
